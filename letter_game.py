0e744e6dbc9925573fea363927e52dc227ec825a
SirFreud <mikevaldez92@gmail.com>
2016-07-31 14:03:50 -0500

    Refactor taking guesses and drawing out guesses in get_gusses() and draw() and clear()

--- letter_game.py
+++ letter_game.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 # Make a list of words
 words = [
 
@@ -14,29 +14,31 @@ words = [
 	'melon'
 ]
 
-number_of_guesses = 7
+def clear():
+	if os.name =='nt':
+		os.system('cls')
+	else:
+		os.system('clear')
 
-while True:
-	start = input("Press Enter to start the game or type Q to quit: ")
-	if start.lower() == 'q':
-		break
+def draw(bad_guesses, good_guesses, secret_word):
+	clear()
+	print('Strikes: {} out of {}'.format(len(bad_guesses), number_of_guesses))
+	print('')
 
-	# Pick a random word - choice method picks a random item out of an iterable
-	secret_word = random.choice(words)
-	bad_guesses = []
-	good_guesses = []
+	for letter in bad_guesses:
+		print(letter, end=' ')
+	print('\n\n')
 
-	while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
-		for letter in secret_word:
-			if letter in good_guesses:
-				print(letter, end='')
-			else:
-				print('_', end='')
+	for letter in secret_word:
+		if letter in good_guesses:
+			print(letter, end='')
+		else:
+			print('_', end='')
 
-		print('')
-		print('Strikes: {} out of {}'.format(len(bad_guesses), number_of_guesses))
-		print('')
+	print('')
 
+def get_guess(bad_guesses, good_guesses):
+	while True:
 		# Take guess
 		guess = input("Guess a letter: ").lower()
 		if len(guess) != 1:
@@ -48,6 +50,24 @@ while True:
 		if not guess.isalpha():
 			print("You can only guess letters!")
 			continue
+		return guess
+		break
+
+number_of_guesses = 7
+while True:
+	start = input("Press Enter to start the game or type Q to quit: ")
+	if start.lower() == 'q':
+		break
+
+	# Pick a random word - choice method picks a random item out of an iterable
+	secret_word = random.choice(words)
+	bad_guesses = []
+	good_guesses = []
+
+	while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
+
+
+
 
 
 		if guess.lower() in secret_word.lower():
