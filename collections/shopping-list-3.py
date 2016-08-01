666c077c9b8bd83f9ce7c7535195a83fb78047e3
SirFreud <mikevaldez92@gmail.com>
2016-07-31 20:41:50 -0500

    Now collecting input on shopping-list-3 for insertion or adding to the list

--- collections/shopping-list-3.py
+++ collections/shopping-list-3.py
@@ -23,6 +23,17 @@ while True:
 		show_help()
 		continue
 	if new_stuff == 'SHOW':
-		show_list
+		show_list()
 		continue
 	new_list = new_stuff.split(',')
+	index =input('Add this at a certain spot? Press Enter for the end of the list or give me a number. Currently {} items in the list'.format(len(shopping_list)))
+	if index:
+		spot = int(index) - 1
+		for item in new_list:
+			shopping_list.insert(spot, item.strip())
+			spot += 1
+	else:
+		for item in new_list:
+			shopping_list.append(item.strip())
+
+
