615447b720f51d497b4a0c75bb3e508846ab5faf
SirFreud <mikevaldez92@gmail.com>
2016-08-03 00:52:53 -0500

    Added a double for loop to remove-vowels.py where I'm searching through each letter in each word. Still not finished

--- collections/remove-vowels.py
+++ collections/remove-vowels.py
@@ -1,21 +1,19 @@
 def main():
 	user_input = input("Please type a list of words separated by commas: ")
 	user_input = user_input.replace(" ", "").split(',')
-	print(user_input)
 	remove_vowels(user_input)
 
 def remove_vowels(list_of_user_input):
 
+	no_vowels = []
+
 	for word in list_of_user_input:
 		for letter in list_of_user_input[word]:
 			if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
-				try:
-					list_of_user_input.remove(letter)
-				except ValueError:
-					continue
+				list_of_user_input.remove(letter)
 			else:
-				list_of_user_input.join(letter)
-				print(list_of_user_input)
-	return list_of_user_input
+				continue
+		no_vowels.append(word)
+	return no_vowels
 
 main()
