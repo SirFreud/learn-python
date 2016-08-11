e0232886288540af7a389f72bd93c4a4b20a62df
SirFreud <mikevaldez92@gmail.com>
2016-08-11 00:05:08 -0500

    Built out the string factory challenge in python

---
+++ collections/string_factory.py
@@ -0,0 +1,22 @@
+dicts = [
+	{'name': 'Michelangelo',
+	 'food': 'PIZZA'},
+	{'name': 'Garfield',
+	 'food': 'lasanga'},
+	{'name': 'Walter',
+	 'food': 'pancakes'},
+	{'name': 'Galactus',
+	 'food': 'worlds'}
+]
+
+string = "Hi, I'm {name} and I love to eat {food}!"
+
+def string_factory(st, di_list):
+	new_list = []
+	for i in di_list:
+		new_list.append(st.format(**i))
+	return new_list
+
+my_variable = string_factory(string, dicts)
+
+print(my_variable)
