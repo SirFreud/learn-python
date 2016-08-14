e9f7d99c54c16d109edeec7d08016cee7d32d0cb
SirFreud <mikevaldez92@gmail.com>
2016-08-13 19:41:13 -0500

    Set the up the map and intiialized the player location on the grid. Created set_map() and starting_player_location functions

--- collections/monster-room.py
+++ collections/monster-room.py
@@ -1,9 +1,16 @@
-from random import *
-the_map = [(1, 2, 3, 4, 5), (1, 2, 3, 4, 5)]
+import random
 
-player_point = the_map[0], the_map[1]
+def main():
+	the_map = set_map()
+	player_point = starting_player_location(the_map)
+	print(player_point)
 
-print (player_point)
+def set_map():
+	return [(1, 2, 3, 4, 5), (1, 2, 3, 4, 5)]
+
+def starting_player_location(the_map):
+	return random.choice(the_map[0]), random.choice(the_map[1])
 
 
+main()
 
