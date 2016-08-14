2e6ae784628d18079ff51a0214168cc5e61addc5
SirFreud <mikevaldez92@gmail.com>
2016-08-14 16:46:52 -0500

    Added in the character class in character.py

---
+++ OOPython/character.py
@@ -0,0 +1,25 @@
+# creating a class for my character
+class Character(object):
+    experience = 0
+    hit_points = 10
+
+# Group common operations into functions
+# Group common functionality into classes
+    def get_weapon(self):
+        weapon_choice = input("Weapon? ([S]word, [A]xe, [B]ow: ").lower()
+        if weapon_choice in 'sab':
+            if weapon_choice == 's':
+                return 'sword'
+            if weapon_choice == 'a':
+                return 'axe'
+            if weapon_choice == 'b':
+                return 'bow'
+        else:
+            return self.get_weapon()
+
+    def __init__(self, **kwargs):
+        self.name = input("Name your character: ")
+        self.weapon = self.get_weapon()
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
