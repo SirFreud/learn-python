80f324cc0bd7caffc8cf14e1617baeea677a6084
SirFreud <mikevaldez92@gmail.com>
2016-08-14 01:32:46 -0500

    Added Goblin subcless to Monster in monster.py

--- OOPython/monster.py
+++ OOPython/monster.py
@@ -2,7 +2,7 @@ import random
 COLORS = ('yellow', 'red', 'blue', 'green')
 
 
-class Monster():
+class Monster(object):
 	min_hit_points = 1
 	max_hit_points = 1
 	min_experience = 1
@@ -11,14 +11,18 @@ class Monster():
 	sound = 'roar'
 	
 	def __init__(self, **kwargs):
-		self.max_hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+		self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
 		self.experience = random.randint(self.min_experience, self.max_experience)
 		self.color = random.choice(COLORS)
 
 		for key, value in kwargs.items():
 			setattr(self, key, value)
-		
-		
+
 	def battlecry(self):
 		return self.sound.upper()
-		
+
+class Goblin(Monster):
+	pass
+	max_hit_points = 3
+	max_experience = 2
+	sound = 'squeak'
