b4df23d40d46e448f476974bdf6fb36e666ed1b9
SirFreud <mikevaldez92@gmail.com>
2016-08-13 21:02:57 -0500

    Creating opening dialog for game

--- monster-game/monster-room.py
+++ monster-game/monster-room.py
@@ -2,9 +2,22 @@ import sys
 import random
 
 def play():
+
+	print("""Welcoem to the Mosnter Room! You've been dropped into a 5x5 grid.\n
+	Your goal is to reach the upper right hand corner at location (5, 5)\n
+	You'll start in a random location\n
+	Watch out! A monster lurks in a random location on the grid.\n
+	If you run into him, you'll die\n
+	You can type any direction you'd like to move: UP, DOWN, LEFT or RIGHT\n
+	Type SHOW LOCATION to find out where on the grid you are.\n
+	Good luck!
+	""")
+
 	the_map = set_map()
 	player_point = starting_player_location(the_map)
 	monster_point = monster_location(the_map)
+
+
 
 	if player_point == monster_point:
 		print("You got eaten by the monster, you lose!")
@@ -32,5 +45,5 @@ def move_up(player_point):
 def end_game():
 	sys.exit()
 
-play()
 
+play()
