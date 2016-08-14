718fd7dd3e3efe9d7f32e67bad103f814b314d37
SirFreud <mikevaldez92@gmail.com>
2016-08-13 21:21:09 -0500

    Finished all core functionality of monster-room.py

--- monster-game/monster-room.py
+++ monster-game/monster-room.py
@@ -9,23 +9,29 @@ def play():
 	monster_point = monster_location(the_map)
 
 	while True:
-		user_input = "\n> ".lower()
+		user_input = input("\n>> ".lower())
 		if user_input == "SHOW LOCATION".lower():
 			show_location(user_input, player_point)
 		if user_input == "UP".lower():
 			move_up(player_point)
-
-
-
-
-
-	if player_point == monster_point:
-		print("You got eaten by the monster, you lose!")
-		end_game()
-
-	# if player_point == (5, 5):
-	# 	print("You win!")
-	# 	return
+		if user_input == "DOWN".lower():
+			move_down(player_point)
+		if user_input == "LEFT".lower():
+			move_left(player_point)
+		if user_input == "RIGHT".lower():
+			move_right(player_point)
+		if user_input == 'QUIT'.lower():
+			end_game()
+		if player_point == monster_point:
+			print("You got eaten by the monster, you lose!")
+			end_game()
+		if player_point == [5, 5]:
+			print("Congratulations! You win!")
+			play_again = input(("Would you like to play again? Y/n: ".lower()))
+			if play_again == 'y':
+				play()
+			else:
+				end_game()
 
 def set_map():
 	return [(1, 2, 3, 4, 5), (1, 2, 3, 4, 5)]
@@ -68,9 +74,8 @@ def move_left(player_point):
 	player_point[0] -= 1
 	return player_point
 
-
-
 def end_game():
+	print("Bye!")
 	sys.exit()
 
 def open_dialog():
@@ -80,8 +85,9 @@ def open_dialog():
 	Watch out! A monster lurks in a random location on the grid.\n
 	If you run into him, you'll die\n
 	You can type any direction you'd like to move: UP, DOWN, LEFT or RIGHT\n
-	If you run to a point off of the grid, you'll fall off!\n
 	Type SHOW LOCATION to find out where on the grid you are.\n
+	If you run to a point off of the grid, you'll fall off!\n
+	Type QUIT to quit the game\n
 	Good luck!
 	""")
 
