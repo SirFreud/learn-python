01af294428d5544eb810447518dae7e52a7ed1cc
SirFreud <mikevaldez92@gmail.com>
2016-08-13 20:53:22 -0500

    Changed the player_point from a tuple to a list that makes changing the values easier

--- monster-game/monster-room.py
+++ monster-game/monster-room.py
@@ -5,9 +5,11 @@ def play():
 	the_map = set_map()
 	player_point = starting_player_location(the_map)
 	monster_point = monster_location(the_map)
-	print(player_point)
-	move_up(player_point)
-	print(player_point)
+
+	if player_point == monster_point:
+		print("You got eaten by the monster, you lose!")
+		end_game()
+
 	# if player_point == (5, 5):
 	# 	print("You win!")
 	# 	return
@@ -16,17 +18,17 @@ def set_map():
 	return [(1, 2, 3, 4, 5), (1, 2, 3, 4, 5)]
 
 def starting_player_location(the_map):
-	return random.choice(the_map[0]), random.choice(the_map[1])
+	return [random.choice(the_map[0]), random.choice(the_map[1])]
 
 def monster_location(the_map):
-	return random.choice(the_map[0]), random.choice(the_map[1])
+	return [random.choice(the_map[0]), random.choice(the_map[1])]
 
 def move_up(player_point):
-	new_location = list(player_point)
-	if new_location[0] > 5:
-		print('You lose!')
+	if player_point[0] == 5:
+		print('You fell off the map! You lose!')
 		end_game()
-	return tuple(new_location)
+	player_point[0] += 1
+	return player_point
 def end_game():
 	sys.exit()
 
