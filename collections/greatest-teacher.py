01760398fd0c0f5e97de9d6dc6eeeaa41ee742f5
SirFreud <mikevaldez92@gmail.com>
2016-08-14 19:22:05 -0500

    Fixing spacing in greatest_teacher.py

--- collections/greatest-teacher.py
+++ collections/greatest-teacher.py
@@ -1,30 +1,38 @@
-teachers = {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'], 'Kenneth Love': ['Python Basics', 'Python Collections', 'Django Basics', 'Django Forms'], 'Michael Valdez': ['All the cool stuff', 'Making cool shit'], 'Bob Jones': ['Bob cant teach for shit']}
+teachers = {
+'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'], 
+'Kenneth Love': ['Python Basics', 'Python Collections', 'Django Basics', 'Django Forms'], 
+'Michael Valdez': ['All the cool stuff', 'Making cool shit'], 
+'Bob Jones': ['Bob cant teach for shit']
+}
 
 
 def most_classes(teachers):
-	highest_classes = ""
-	temp = 0
-	for value in teachers:
-		if len(teachers[value]) > temp:
-			temp = len(teachers[value])
-			highest_classes = value
-	return highest_classes
+    highest_classes = ""
+    temp = 0
+    for value in teachers:
+        if len(teachers[value]) > temp:
+            temp = len(teachers[value])
+            highest_classes = value
+    return highest_classes
+
 
 def num_teachers(teachers):
-	total_num = len(teachers)
-	return total_num
+    total_num = len(teachers)
+    return total_num
+
 
 def stats(teachers):
-	teacher_list = []
-	for key in teachers:
-		new_string = [key, len(teachers[key])]
-		teacher_list.append(new_string)
-	return teacher_list
+    teacher_list = []
+    for key in teachers:
+        new_string = [key, len(teachers[key])]
+        teacher_list.append(new_string)
+    return teacher_list
+
 
 def courses(teachers):
     course_list = []
     for courses in teachers:
-    	course_list.extend(teachers[courses])
+        course_list.extend(teachers[courses])
     return course_list
 
 print(courses(teachers))
